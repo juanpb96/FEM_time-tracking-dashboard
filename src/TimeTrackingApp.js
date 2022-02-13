e1ef5e40f67bbbaf5f01c9f812a5951db6aae8f9
MasterDev <juanpb.96@gmail.com>
2022-02-12 19:16:08 -0500

    Update main structure

--- src/TimeTrackingApp.js
+++ src/TimeTrackingApp.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
+
+import { UserCard } from "./components/user/UserCard";
+import { CardList } from "./components/card/CardList";
 
 
 export const TimeTrackingApp = () => {
 
+    const [timeframe, setTimeframe] = useState('weekly');
+
+    const handleTimeframeChange = (newTimeframe) => {
+        setTimeframe(newTimeframe);
+    }
+
     return (
-        <h1>Time tracking dashboard</h1>
+        <>
+            <h1>Time tracking dashboard</h1>
+            <UserCard 
+                handleTimeframeChange={ handleTimeframeChange } 
+            />
+            <CardList 
+                timeframe={ timeframe }
+            />
+        </>
     )
 
 };
