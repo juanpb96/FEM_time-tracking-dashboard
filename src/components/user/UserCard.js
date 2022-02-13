67eb4657b6cff7a76a7e14e3b50eae6fde1501f6
MasterDev <juanpb.96@gmail.com>
2022-02-13 17:34:23 -0500

    Update structure and styles for mobile view

--- src/components/user/UserCard.js
+++ src/components/user/UserCard.js
@@ -3,14 +3,16 @@ import './user.scss';
 export const UserCard = ({handleTimeframeChange}) => {
     return (
         <div className="user">
-            <div className="user__info">
+            <div className="user__content">
                 <img 
                     className="user__picture" 
                     src="/assets/image-jeremy.png" 
                     alt="Jeremy's profile"
                 />
-                <h3>Report for</h3>
-                <h2 className="user__name"><span>Jeremy</span> <span>Robson</span></h2>
+                <div className="user__info">
+                    <h3 className='user__title'>Report for</h3>
+                    <h2 className="user__name"><span>Jeremy</span> <span>Robson</span></h2>
+                </div>
             </div>
             <div className="user__timeframes">
                 <button type="button" onClick={ () => handleTimeframeChange('daily') }>Daily</button>
