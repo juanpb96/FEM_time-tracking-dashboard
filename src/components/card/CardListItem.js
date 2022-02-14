80a2c35e03f0c7eadf6c383ff112f0d7cf223306
MasterDev <juanpb.96@gmail.com>
2022-02-13 21:20:56 -0500

    Update card structure and formatted text for day

--- src/components/card/CardListItem.js
+++ src/components/card/CardListItem.js
@@ -1,4 +1,4 @@
-
+import './cardListItem.scss';
 
 export const CardListItem = ({selectedTimeframe, title, timeframes}) => {
     
@@ -8,9 +8,16 @@ export const CardListItem = ({selectedTimeframe, title, timeframes}) => {
     return (
         <div className="card">
             <div className="card__content">
-                <h3 className="card__title">{ title }</h3>
-                <h2 className="card__current">{ current }hrs</h2>
-                <p className="card__previous">Last { formattedTimeframe === 'dai' ? 'day' : formattedTimeframe } - { previous }hrs</p>
+                <h3 className="card__title"><span>{ title }</span></h3>
+                <div className="card__timeframe">
+                    <h2 className="card__current">{ current }hrs</h2>
+                    <p className="card__previous">
+                    { 
+                        formattedTimeframe === 'dai' 
+                            ? 'Yesterday' 
+                            : 'Last ' + formattedTimeframe 
+                    } - { previous }hrs</p>
+                </div>
             </div>
         </div>
     );
