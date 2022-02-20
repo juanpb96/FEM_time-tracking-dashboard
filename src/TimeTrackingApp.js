16bf3a25d95c19d54a7f5016be7e54af3e8d9234
MasterDev <juanpb.96@gmail.com>
2022-02-20 18:42:11 -0500

    Update structure and send timeframe value to UserCard

--- src/TimeTrackingApp.js
+++ src/TimeTrackingApp.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { UserCard } from "./components/user/UserCard";
 import { CardList } from "./components/card/CardList";
+import './styles.scss';
 
 
 export const TimeTrackingApp = () => {
@@ -13,15 +14,16 @@ export const TimeTrackingApp = () => {
     }
 
     return (
-        <>
-            <h1>Time tracking dashboard</h1>
+        <main>
+            <h1 className="sr-only">Time tracking dashboard</h1>
             <UserCard 
+                timeframe={ timeframe }
                 handleTimeframeChange={ handleTimeframeChange } 
             />
             <CardList 
                 timeframe={ timeframe }
             />
-        </>
+        </main>
     )
 
 };
